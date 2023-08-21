826af69b5a7c1cfe741a4845a29febc392fdb667
Mirzayev96 <mirzayevmm96@gmail.com>
2023-08-21 17:02:15 +0400

    first_commit

---
+++ ders1.py
@@ -0,0 +1,76 @@
+
+# a= int(input( 'eded daxil edin'))
+
+# if a%2 ==0:
+#     print('bu eded cut ededdir')
+# else:
+#     print('bu eded tek ededdir')
+
+# i = 0
+# n= 5
+# while 1<10:
+#     i+=1
+#     c = n*i
+
+# i= 0
+# while i<10:
+#    i+=1 
+#    n = 0
+#    while n<10:
+#     n += 1
+#     c= n*i
+#     print(str(i) + ' * ' + str(n) + ' = ' + str(c))
+
+
+# a = int(input( ' eded daxil edin '))
+# if a%2 ==0:
+#     print( ' cut ')
+# else:
+#     print(' tek ')
+
+# a = 0
+# b = 5
+# while a<10:
+#     a+=1
+#     c = a*b
+#     print(str(b ) + ' * ' + str(a) + ' = ' + str(c))
+
+a= 0
+while a<10:
+    a+=1
+    b = 0
+    while b<10:
+       b+=1
+       c = a*b
+       print(str(a) , ' * ' , str(b) , ' = ' , str(c))
+
+# boy = int(input( ' boy yaz '))
+# kilo = int(input( ' ceki yaz '))
+# bki = (kilo / (boy**2))*100
+
+# if bki < 18.5:
+#     print('zeyif')
+# elif bki >= 18.5 and bki < 25:
+#     print('normal')
+# elif bki >= 25 and bki < 30:
+#     print('cox')
+# else:
+#     print('cox cox')
+
+
+# a = 1; b= 2; c= 3; d = 4
+# a,b,c,d = range(1,5)
+# print(a,b,c,d)
+
+# a = 2; b = 10
+# a,b = b,a
+# print(a,b)
+
+# qiymet = float(input('qiymeti daxil et: '))
+# endirimfaizi = float(input( ' endirim faizini daxil et: '))
+# yekunqiymet = qiymet- qiymet * endirimfaizi/100
+# print('yekun qiymet :', yekunqiymet, 'azn')
+
+# print('adil' , 'sevil' , 'rena' , end=' telebeler ')
+
+# print('www', 'virtual-kurs' , 'com', 
---
+++ python/def.py
@@ -0,0 +1,17 @@
+# def my_function(country = 'norway'):
+#     print( ' i am from ' + country)
+
+
+# my_function('sweden')
+# my_function( 'india')
+# my_function()
+# my_function('brazil')
+
+# def a (x):
+#     print(x)
+# a()
+
+# def a(*args):
+#     print('his last name' + args(0))
+
+# a('python' , 'java' , "php")
